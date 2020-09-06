1b03175f4af3632f675c0cd38aa205e3e46b747a
Shubhamj08 <sj951563@gmail.com>
2020-09-06 23:31:49 +0530

    post ideas feature added

--- backend/routes/ideas.js
+++ backend/routes/ideas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const Idea = mongoose.model(
   "Idea",
@@ -12,6 +13,26 @@ const Idea = mongoose.model(
   })
 );
 
+const ideaPostSchema = Joi.object({
+  title: Joi.string().min(5).required(),
+  description: Joi.string().required(),
+});
+
+function addToDb(req, res) {
+  const idea = new Idea({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  const dbSaveResult = idea.save();
+}
+
+async function renderIdeas(req, res) {
+  let ideas = await Idea.find();
+  res.render("ideas", {
+    ideas: ideas,
+  });
+}
+
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -22,10 +43,17 @@ router.use(function (req, res, next) {
 });
 
 router.get("/", async (req, res) => {
-  const ideas = await Idea.find();
-  res.render("ideas", {
-    ideas: ideas,
-  });
+  renderIdeas(req, res);
+});
+
+router.post("/", async (req, res) => {
+  const result = ideaPostSchema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error);
+    return;
+  }
+  addToDb(req, res);
+  renderIdeas(req, res);
 });
 
 module.exports = router;
--- script.js
+++ script.js
@@ -2,4 +2,22 @@ $(document).ready(() => {
   $.get("http://localhost:3000/api/ideas", (data, status) => {
     $("#ideas-container").append(data);
   });
+
+  $("#submit-post").click(function () {
+    const idea = {
+      title: $("#title").val(),
+      description: $("#description").val(),
+    };
+    console.log(JSON.stringify(idea));
+    $.ajax({
+      type: "POST",
+      data: JSON.stringify(idea),
+      contentType: "application/json",
+      url: "http://localhost:3000/api/ideas",
+      success: function (data) {
+        console.log(JSON.stringify(data));
+      },
+    });
+    location.reload(true);
+  });
 });
