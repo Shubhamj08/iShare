c1df32c14b15606f697c6e451938a9c273ae0204
Shubhamj08 <sj951563@gmail.com>
2021-04-11 10:43:10 +0530

    fixed bugs

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
     mongoose
-        .connect("mongodb://localhost:27017/ishare")
+        .connect("mongodb://localhost:27017/ishare", {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
         .then(() => console.log("connected to MongoDb"))
         .catch((err) => console.error(`could not connect to mongoDB... err: ${err}`));
 }
