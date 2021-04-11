af99f8f165072c4a5bb24ed7911ded9639da326a
Shubhamj08 <sj951563@gmail.com>
2021-04-11 13:28:57 +0530

    fixed mogodb issues

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -3,10 +3,10 @@ const config = require("config");
 
 module.exports = function () {
     mongoose
-        .connect(config.get("mongoDbUrl"), {
+        .connect(`mongodb+srv://shubham:${config.get("mongoDbUrl")}@isharedb.d6ql0.mongodb.net/ishare`, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         })
-        .then(() => console.log("connected to MongoDb"))
+        .then(() => console.log("connected to mongoDB"))
         .catch((err) => console.error(`could not connect to mongoDB... err: ${err}`));
 }
