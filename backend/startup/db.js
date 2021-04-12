358670147d4a294ff2778182bc8850bb593b97fc
Shubhamj08 <sj951563@gmail.com>
2021-04-12 22:29:05 +0530

    -

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -6,6 +6,7 @@ module.exports = function () {
         .connect(`mongodb+srv://shubham:${config.get("mongoDbUrl")}@isharedb.d6ql0.mongodb.net/ishare?retryWrites=true&w=majority`, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
+            useCreateIndex: true
         })
         .then(() => console.log("connected to mongoDB"))
         .catch((err) => console.error(`could not connect to mongoDB... err: ${err}`));
