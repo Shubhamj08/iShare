c6d693d243aaa4bbf3df2b8d5855a75ee13b72c2
Shubhamj08 <sj951563@gmail.com>
2021-05-26 21:42:35 +0530

    -

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -5,15 +5,17 @@ const auth = require("../routes/auth");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
+    app.set('view engine', 'pug');
+    app.set('views', './views');
     app.use(express.json());
     app.use("/api/ideas", ideas);
     app.use("/api/users", users);
     app.use("/api/auth", auth);
     app.get("/", (req, res) => {
-        res.send("ISHARE Backend server");
+        res.render('serverpage');
     });
     app.get("/api", (req, res) => {
-        res.send("Use /ideas, /users, /auth endpoints");
+        res.render('apipage');
     });
     app.use(error);
 }
